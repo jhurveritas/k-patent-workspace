bf3c29e035106f9480447e98e1bbb03af6531e85
jhurveritas <sksmsgjwjd@gmail.com>
2026-02-23 12:27:47 +0900

    Update gemini.js

--- functions/api/gemini.js
+++ functions/api/gemini.js
@@ -10,7 +10,7 @@ export async function onRequest(context) {
     }
 
     const requestBody = await context.request.json();
-    const googleUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+    const googleUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-pro:generateContent?key=${apiKey}`;
     
     const googleResponse = await fetch(googleUrl, {
       method: 'POST',
@@ -34,5 +34,6 @@ export async function onRequest(context) {
   }
 
 }
+
 
 
